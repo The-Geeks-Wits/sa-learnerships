449f741c17a6257314f048fdd9ec51a03ecc1db3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-04-16 08:34:40 +0200

    Fix how we access the environment variable

--- env.config.js
+++ env.config.js
@@ -1,8 +1,5 @@
 export const backendURL = () => {
-    const environment = 'dev'; // The environment the app is under
-    if (!environment || (environment && environment === 'dev')) {
-        return 'http://localhost:3000';
-    } else if (environment && environment === 'prod') {
-        return 'https://salearnerships.azurewebsites.net';
-    }
+    const environment = 'prod'; // The environment the app is under
+    if (environment === 'dev') return 'http://localhost:3000';
+    else if (environment === 'prod') return 'https://salearnerships.azurewebsites.net';
 };
