47bf78cbe73c44f3aa54a0412042557e1bdf7510
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-04-17 14:22:35 +0200

    Test opportunities

---
+++ backend/__tests__/opportunities/getOpportunity.test.js
@@ -0,0 +1,197 @@
+const controller = require('../../opportunities/controller.js');
+const Opportunity = require('../../opportunities/Opportunity.js');
+
+jest.mock('../../opportunities/Opportunity.js', () => ({
+    findById: jest.fn(),
+}));
+
+describe('Get Opportunity', () => {
+    it('should return a 400 status when there are no params', async () => {
+        // Mock request object
+        const req = {};
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should return an object with an error property when there are no params', async () => {
+        // Mock request object
+        const req = {};
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        const json = res.json.mock.calls[0][0];
+        expect(json.error).toBeDefined();
+    });
+
+    it('should return a 400 status when params do not have an id property', async () => {
+        // Mock request object
+        const req = { params: {} };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should return an object with an error property when params do not have an id property', async () => {
+        // Mock request object
+        const req = { params: {} };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        const json = res.json.mock.calls[0][0];
+        expect(json.error).toBeDefined();
+    });
+
+    it('should not return an object with an error property when params do not have an id property', async () => {
+        // Mock request object
+        const req = { params: {} };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        const json = res.json.mock.calls[0][0];
+        expect(json.error).toBeDefined();
+    });
+
+    it('should call the Opportunity.findById method when params have an id property', async () => {
+        // Mock request object
+        const req = { params: { id: 'test-id' } };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        expect(Opportunity.findById).toHaveBeenCalled();
+    });
+
+    it('should use the correct param id to fetch opportunity details', async () => {
+        // Mock request object
+        const req = { params: { id: 'test-id' } };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        // Opportunity gets mocked so we are not using the schema object
+        expect(Opportunity.findById).toHaveBeenCalledWith(req.params.id);
+    });
+
+    it('should return a 400 status when the opportunity is not found', async () => {
+        // Opportunity gets mocked, hence the mockResolvedValue function exists
+        Opportunity.findById.mockResolvedValue(undefined);
+
+        // Mock request object
+        const req = { params: { id: 'test-id' } };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should return an object with an error property when the opportunity is not found', async () => {
+        // Opportunity gets mocked, hence the mockResolvedValue function exists
+        Opportunity.findById.mockResolvedValue(undefined);
+
+        // Mock request object
+        const req = { params: { id: 'test-id' } };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        const json = res.json.mock.calls[0][0];
+        expect(json.error).toBeDefined();
+    });
+
+    it('should return a 200 status on success', async () => {
+        // Opportunity gets mocked, hence the mockResolvedValue function exists
+        Opportunity.findById.mockResolvedValue({});
+
+        // Mock request object
+        const req = { params: { id: 'test-id' } };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    it('should return a 500 status on error', async () => {
+        // Opportunity gets mocked, hence the mockRejectedValue function exists
+        Opportunity.findById.mockRejectedValue(new Error('Test error'));
+
+        // Mock request object
+        const req = { params: { id: 'test-id' } };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+    });
+
+    it('should return an object with an error property on error', async () => {
+        // Opportunity gets mocked, hence the mockRejectedValue function exists
+        Opportunity.findById.mockRejectedValue(new Error('Test error'));
+
+        // Mock request object
+        const req = { query: {} };
+
+        // Mock response object
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await controller.getOpportunity(req, res);
+        const json = res.json.mock.calls[0][0];
+        expect(json.error).toBeDefined();
+    });
+});
