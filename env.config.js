df23732996377c1979cd5bc08f02414cd0c42072
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-04-21 21:03:55 +0200

    Ignore the env.config.js file on github commits

--- env.config.js
+++
@@ -1,5 +0,0 @@
-export const backendURL = () => {
-    const environment = 'prod'; // The environment the app is under
-    if (environment === 'dev') return 'http://localhost:3000';
-    else if (environment === 'prod') return 'https://salearnerships.azurewebsites.net';
-};
