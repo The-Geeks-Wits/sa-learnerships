c591a1b532d0df1519f8cf0e9286554e5d22dd16
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-04-22 08:45:23 +0200

    Implement change of tabs

--- profile/scripts/view.js
+++ profile/scripts/view.js
@@ -28,15 +28,37 @@ const getPersonalDetailsElement = (user) => {
 };
 
 const showPersonalDetails = (user) => {
-    console.log('personal');
+    personalTab.classList.add('visible');
+    educationTab.classList.remove('visible');
+    skillsTab.classList.remove('visible');
+    console.log(user);
+
+    visibleDetails.innerHTML = `<ul id="visible-details">
+        <li>
+            <h4>Location</h4>
+            <p>${user.location || 'Not provided'}</p>
+        </li>
+        <li>
+            <h4>Phone</h4>
+            <p>${user.phone || 'Not provided'}</p>
+        </li>
+    </ul>`;
 };
 
 const showEducationDetails = (user) => {
-    console.log('education');
+    personalTab.classList.remove('visible');
+    educationTab.classList.add('visible');
+    skillsTab.classList.remove('visible');
+
+    visibleDetails.innerHTML = '';
 };
 
 const showSkillsDetails = (user) => {
-    console.log('skills');
+    personalTab.classList.remove('visible');
+    educationTab.classList.remove('visible');
+    skillsTab.classList.add('visible');
+
+    visibleDetails.innerHTML = '';
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -58,5 +80,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         personalTab.addEventListener('click', () => showPersonalDetails(data.user));
         educationTab.addEventListener('click', () => showEducationDetails(data.user));
         skillsTab.addEventListener('click', () => showSkillsDetails(data.user));
+
+        // Start with personal details
+        showPersonalDetails(data.user);
     }
 });
