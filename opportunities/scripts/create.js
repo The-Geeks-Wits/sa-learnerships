4bac0ed14c2c5e033ab1fa9e502a7fae19d1925a
MufasaTryingCOMS <2560516@students.wits.ac.za>
2026-05-20 14:05:47 +0200

    fix(opportunities): include sector value in create opportunity request

--- opportunities/scripts/create.js
+++ opportunities/scripts/create.js
@@ -12,7 +12,7 @@ const closingDate = document.getElementById('closing-date');
 const errorMessage = document.getElementById('error-message');
 const submitBtn = document.getElementById('submit-btn');
 const addRequirementsBtn = document.getElementById('add-requirement-btn');
-const sector = document.getElementById('sector').value;
+const sector = document.getElementById('sector');
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -63,6 +63,7 @@ form.addEventListener('submit', async (event) => {
                 duration: duration.value,
                 stipend: stipend.value,
                 location: locationElement.value,
+                sector: sector.value,
             }),
         });
 
