fb4008569cd64ea31ffe90d5ed889b558aa5bb23
MufasaTryingCOMS <2560516@students.wits.ac.za>
2026-05-21 23:32:01 +0200

    fix(config): update prod URL to correct Azure deployment URL

--- env.config.js
+++ env.config.js
@@ -1,5 +1,5 @@
 export const backendURL = () => {
     const environment = 'prod'; // The environment the app is under
     if (environment === 'dev') return 'http://localhost:3000';
-    else if (environment === 'prod') return 'https://salearnerships.azurewebsites.net';
+    else if (environment === 'prod') return 'https://sa-learnerships-aadwawdsbyhngnc5.southafricanorth-01.azurewebsites.net';;
 };
